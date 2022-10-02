a278a2bad40ca1fbe4cef676350eb4223739dbf4
Your Name <97912793+1Anka1@users.noreply.github.com>
2022-10-02 11:55:52 +0200

    Update task-1

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,23 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos : []
+  }
+
+  handleChange = search => {
+    const { addTodo } = this;
+    addTodo(search);
+    };
+  
+  addTodo = text => {
+    const todo = { id: nanoid(), text }
+    this.setState(prev =>({
+      todos : [...prev.todos, todo]
+    }))
+  }
+
   render() {
-    return <Text>Todos</Text>;
+    return <SearchForm onChange={this.handleChange} />;
   }
 }
