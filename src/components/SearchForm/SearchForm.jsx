5490ace2a59c8dd22ab1cd97089dbd595e2d96a1
Sarat862 <sarat862@gmail.com>
2022-10-02 11:11:35 +0300

    Done task-1-1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,33 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+
+  state = {
+    search: "",
+  }
+
+  handleChange = (e) => {
+    this.setState(
+      {search: e.target.value.trim().toLowerCase()}
+    )
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onChange(
+      this.state.search
+    )
+    this.setState(
+      {
+    search: "",
+  }
+    )
+  }
+
   render() {
-    return <h2>SearchForm</h2>;
+    return <SearchFormStyled onSubmit={this.handleSubmit}>
+      <InputSearch onChange={this.handleChange} value={this.state.search} />
+      <FormBtn><FiSearch /></FormBtn> 
+    </SearchFormStyled>;
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,21 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+
+  state = {
+    search: ""
+  }
+
+  handleChange = (search) => {
+    this.setState({
+      search 
+    })
+  }
+
   render() {
     return (
       <>
+        <SearchForm onChange={this.handleChange} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
